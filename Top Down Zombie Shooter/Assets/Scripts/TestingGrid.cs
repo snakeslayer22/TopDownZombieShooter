9d707df4c1396226dd93bc138deec9f4a9753a2b
snakeslayer22 <66408223+snakeslayer22@users.noreply.github.com>
2020-06-07 00:44:10 +1000

    added codeMonkeyUtils and doing pathfinding

---
+++ Top Down Zombie Shooter/Assets/Scripts/TestingGrid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class TestingGrid : MonoBehaviour{
+
+    private void Start()
+    {
+        Grid grid = new Grid(4, 2);
+    }
+}
